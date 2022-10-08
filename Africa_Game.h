4f0617dfe1c1589401a3a63816d5489a33f6c483
Clockiieee <101518502+Clockiieee@users.noreply.github.com>
2022-10-08 01:24:36 -0400

    Create Africa_Game.h

---
+++ Africa_Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+
+void GameRules();
+void GameOptions();
+void GamePuzzle();
+void GameDecision(char a);
+void getGameOption(int opt);
+
+class GameCountry;
+class GameLetter;
+class GameHint;
+class GameMessage;
